01eec8219b0dcd298dd23d44268a92f415884d9c
Girish <girishk1992@gmail.com>
2026-07-12 10:41:43 +0530

    Datatype programming-list-tuple

---
+++ pythonProject/Durgasoft_Python_practice/list_Items.py
@@ -0,0 +1,14 @@
+list = [10,20,30]
+print(list)
+for num in list:
+    print(num)
+
+result = list.append(40)
+print(result)
+print(list)
+
+list.remove(20)
+print(list)
+
+list.append("DurgaSoft")
+print(list)
---
+++ pythonProject/Durgasoft_Python_practice/tuple_items.py
@@ -0,0 +1,4 @@
+t = (10,20,30,40)
+print(type(t))
+for t in t:
+    print(t)
