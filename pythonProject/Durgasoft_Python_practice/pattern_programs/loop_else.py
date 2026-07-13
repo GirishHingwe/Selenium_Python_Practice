209da58760687106b2620f62aa5b3e5e1fd191f7
Girish <girishk1992@gmail.com>
2026-07-13 12:21:52 +0530

    pattern_program

---
+++ pythonProject/Durgasoft_Python_practice/pattern_programs/loop_else.py
@@ -0,0 +1,17 @@
+# cart = [10,20,30,40,50]
+# for item in cart:
+#     if item>= 500:
+#         print("We cannot process this order")
+#         break
+#     print(item)
+# else:
+#     print("Congrats ... all items processed successfully")
+
+cart = [10,20,600,30]
+for item in cart:
+    if item>=500:
+        print("We cannot process this order")
+        break
+    print(item)
+else:
+    print("Congrats ...all items processed successfully")
