6879b6ac514fe9d91953015e7ceacbc47e5ec49e
Girish <girishk1992@gmail.com>
2026-07-13 12:21:01 +0530

    update-13-26

---
+++ pythonProject/Durgasoft_Python_practice/conditions/Enter_name.py
@@ -0,0 +1,4 @@
+name = ""
+while name != "durga":
+    name = input("Enter name: ")
+print("Thanks for confirmation")
---
+++ pythonProject/Durgasoft_Python_practice/conditions/slice_p.py
@@ -0,0 +1,9 @@
+s = 'abcdefghij'
+print(s[1:6:2])
+print(s[0:9:3])
+print(s[::1])
+print(s[::2])
+print(s[::3])
+print(s[::-1])
+print(s[3:7:-1])
+print(s[-4:1:-1])
